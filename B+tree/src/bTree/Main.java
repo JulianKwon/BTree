ab874b37beec535d08dfc99acbcd3a9d2a691979
imjul <imjul@BioComJulian>
2016-05-10 17:37:39 +0900

    second node

--- B+tree/src/bTree/Main.java
+++ B+tree/src/bTree/Main.java
@@ -2,16 +2,28 @@ package bTree;
 
 public class Main
 {
-	private class Node{
-		private Entry[] children = new Entry[4];
-	}
+	Node root;
 	
-	private class Entry{
-		private int key;
-		private Node next;
-		public Entry(int key, Node next){
-			this.key = key;
-			this.next = next;
+	private class Node{
+		private int k1;
+		private int k2;
+		private int k3;
+		private int k4;
+		private Node c1;
+		private Node c2;
+		private Node c3;
+		private Node c4;
+		private Node c5;
+		public Node(int k1, int k2, int k3, int k4, Node c1, Node c2, Node c3, Node c4, Node c5){
+			this.k1 = k1;
+			this.k2 = k2;
+			this.k3 = k3;
+			this.k4 = k4;
+			this.c1 = c1;
+			this.c2 = c2;
+			this.c3 = c3;
+			this.c4 = c4;
+			this.c5 = c5;
 		}
 	}
 
