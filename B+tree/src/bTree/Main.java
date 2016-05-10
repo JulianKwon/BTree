03d6bc0168cac947cc5a0f02b9baa2ef507f9b74
imjul <imjul@BioComJulian>
2016-05-10 17:37:22 +0900

    first node

--- B+tree/src/bTree/Main.java
+++ B+tree/src/bTree/Main.java
@@ -2,6 +2,18 @@ package bTree;
 
 public class Main
 {
+	private class Node{
+		private Entry[] children = new Entry[4];
+	}
+	
+	private class Entry{
+		private int key;
+		private Node next;
+		public Entry(int key, Node next){
+			this.key = key;
+			this.next = next;
+		}
+	}
 
 	public static void main(String[] args)
 	{
--- B+tree/src/bTree/MkTree.java
+++ B+tree/src/bTree/MkTree.java
@@ -2,5 +2,8 @@ package bTree;
 
 public class MkTree
 {
-
+	void insert(int key)
+	{
+		
+	}
 }
