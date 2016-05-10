f767df4e87e2f4c93924ba0a98394836d01c5106
imjul <imjul@BioComJulian>
2016-05-10 13:49:37 +0900

    format

--- B+tree/src/bTree/Main.java
+++ B+tree/src/bTree/Main.java
@@ -1,10 +1,11 @@
 package bTree;
 
-public class Main {
+public class Main
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 
 	}
-
 }
