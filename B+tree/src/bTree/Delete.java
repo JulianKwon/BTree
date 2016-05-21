7a8a38e71f9ae3e2cdfcbf87ec103495cd862465
imjul <imjul@BioComJulian>
2016-05-21 17:35:22 +0900

    0

--- B+tree/src/bTree/Delete.java
+++ B+tree/src/bTree/Delete.java
@@ -4,8 +4,15 @@ public class Delete extends NodeCreate
 {
 	Node root = Main.root;
 	
-	public static void deletekey(int key)
+	public static void deletekey(int key, Node x)
 	{
-		
+		int i = x.getsize() - 1;
+		while (i >= 0 && key < x.getkey(i))
+				i--;
+			i++;
+		if(!x.isleaf())
+		{
+			Node y = 
+		}
 	}
 }
